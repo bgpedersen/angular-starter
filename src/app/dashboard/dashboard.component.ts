7b62136a42757bb329d332481eb08e3b17bc0da1
Bjarne <bgp@proreact.dk>
2018-03-19 14:58:58 +0100

    Using lodash

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,11 +9,16 @@ import * as moment from 'moment';
 })
 export class DashboardComponent implements OnInit {
   time: string;
+  nonUniqArray = [5, 3, 6, 1, 1, 5, 2];
+  uniqArray = [];
 
   constructor() { }
 
   ngOnInit() {
+    // Moment
     this.time = moment().format('HH:mm DD/MM/YYYY');
+    // Lodash
+    this.uniqArray = _.uniq(this.nonUniqArray);
   }
 
 }
